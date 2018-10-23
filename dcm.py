3b75c36f4c31059f95e7ccc1c7c67b627ce57aba
rabieo <43918617+rabieo@users.noreply.github.com>
2018-10-22 21:01:08 -0400

    changed some colors

--- dcm.py
+++ dcm.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import tkinter.messagebox
+#from tkinter.ttk import * # makes bottons look better
 
 root = Tk()
 
@@ -9,26 +10,22 @@ def newuser():
     with open('usernames.txt', 'a') as f:
         f.write(entry1.get())
         f.write('\n')
-        
         f.close()
+        entry1.delete(0, 'end') # removes the text from entry after u press register
 
 
 root.config(background="light blue")
-root.geometry("500x600")
+root.geometry("500x500")
 root.title("beatsronix")
 
 
 
-welcome = Label(text = "Welcome to BeatTronix", font=('none', 25)).place(x = 100, y = 60)
-
-#photo = PhotoImage(file="pmm.png")
-#pic = Label(root, image = photo)
-#pic.place( x = 100, y = 110)
+welcome = Label(text = "Welcome to BeatTronix", background="light blue", font=('none', 25)).place(x = 100, y = 60)
 
 
 
-label_1 = Label(text ="Username").place(x=207, y=277)
-label_2 = Label(text ="Password").place(x = 210, y=300)
+label_1 = Label(text ="Username", background="light blue").place(x=207, y=277)
+label_2 = Label(text ="Password", background="light blue").place(x = 210, y=300)
 
 entry1 = Entry(root)
 entry1.place(x=270, y =277)
@@ -37,8 +34,8 @@ entry2.place(x=270, y =300)
 
 
 
-login = Button(text = "Login").place(x = 270, y = 340)
-register = Button(text = "Register a new user", command = newuser).place( x = 240 , y = 430)
+login = Button(text = "Login", bg="light blue").place(x = 270, y = 340)
+register = Button(text = "Register a new user", command = newuser, bg="light blue").place( x = 240 , y = 430)
 
 
 
